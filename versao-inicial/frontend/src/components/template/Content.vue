683d753b7dfd06c008eb27b68faa6f080d3ebca9
MarcalFilosofo <58859672+MarcalFilosofo@users.noreply.github.com>
2021-03-06 21:05:52 -0300

    20210306

---
+++ versao-inicial/frontend/src/components/template/Content.vue
@@ -0,0 +1,19 @@
+<template>
+	<div id="content">
+		aaa
+	</div>
+</template>
+
+<script>
+export default {
+	name: 'Content'
+}
+</script>
+
+<style>
+	.content {
+        grid-area: content;
+        background-color: #efefef;
+        padding: 20px;
+    }
+</style>
---
+++ versao-inicial/frontend/src/components/template/Footer.vue
@@ -0,0 +1,25 @@
+<template>
+	<footer class="footer">
+		<span>Feito por <strong>Guilherme Marçal</strong> 2021</span>
+	</footer>
+</template>
+
+<script>
+export default {
+	name: 'Footer'
+}
+</script>
+
+<style>
+	.footer{
+		grid-area: footer;
+		background-color: #ddd;
+		color: #333;
+
+		display: flex;
+		flex-direction: row;
+		align-items: center;
+		justify-content: flex-end;
+		padding-right: 10px;
+	}
+</style>
